7d804caf51d848e5d75e36fcf6e5b6559d5a5b46
Candido Acevedo <candidoacevedodev@gmail.com>
2022-11-16 18:07:36 -0400

    Proyecto finalizado 100% responsive ❤

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+            iconEye = document.getElementById(inputIcon);
+
+    iconEye.addEventListener('click', () => {
+        // Change password to text
+        if(input.type === 'password'){
+            // Switch to text
+            input.type = 'text'
+
+            // Add icon and remove icon
+            iconEye.classList.remove('ri-eye-off-line')
+            iconEye.classList.add('ri-eye-line')
+        }else{
+
+            // Switch to text
+            input.type = 'text'
+            input.type = 'password'
+
+            // Add icon and remove icon
+            iconEye.classList.remove('ri-eye-line')
+            iconEye.classList.add('ri-eye-off-line')
+            
+        }
+    })
+}
+
+showHiddenPass('password', 'input-icon')
